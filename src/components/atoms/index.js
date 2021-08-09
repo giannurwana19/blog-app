86d4ce3f1045bee7e7a98d439491e2984358ec29
giannurwana19 <giannurwana19@gmail.com>
2021-08-09 19:20:50 +0700

    slicing register page

---
+++ src/components/atoms/index.js
@@ -0,0 +1,8 @@
+import Input from './Input';
+import Button from './Button';
+import Gap from './Gap';
+import Link from './Link';
+
+export { Input, Button, Gap, Link };
+
+// secara default kita tidak boleh memiliki margin di dalam comp atoms
