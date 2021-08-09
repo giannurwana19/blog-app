1b3cb4063328b69d4beaf1e79618a8fea32b4506
giannurwana19 <giannurwana19@gmail.com>
2021-08-09 20:05:03 +0700

    Routing bertingkat pada MainApp page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Home from './Home';
 import Login from './Login';
+import MainApp from './MainApp';
 import Register from './Register';
 
-export { Home, Login, Register };
+export { Home, Login, Register, MainApp };
