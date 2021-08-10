c409f2aaf130129db946ec38daac1608e47a5ebc
giannurwana19 <giannurwana19@gmail.com>
2021-08-10 20:10:16 +0700

    mengubah value dari state global redux

--- src/config/redux/store.js
+++ src/config/redux/store.js
@@ -1,11 +1,24 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  dataBlog: [],
+  dataBlogs: [],
   name: 'Gian',
 };
 
 const reducer = (state = initialState, action) => {
+  if (action.type === 'UPDATE_DATA_BLOG') {
+    return {
+      ...state,
+      dataBlogs: action.payload,
+    };
+  }
+
+  if (action.type === 'UPDATE_NAME') {
+    return {
+      ...state,
+      name: 'Nurwana',
+    };
+  }
   return state;
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,31 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { BlogItem, Button, Gap } from '../../components';
+import axios from 'axios';
 import './home.scss';
 
 const Home = () => {
   const history = useHistory();
-  const [dataBlog, setDataBlog] = useState([]);
-  const stateGlobal = useSelector(state => state);
+  const { dataBlogs, name } = useSelector(state => state);
+  const dispatch = useDispatch();
+  // const [dataBlog, setDataBlog] = useState([]);
 
-  console.log('state global', stateGlobal);
+  console.log('data blog global', dataBlogs);
 
   useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: 'UPDATE_NAME' });
+    }, 3000);
+
     axios
-      .get('http://localhost:4000/v1/blog/posts')
+      .get(`${process.env.REACT_APP_URL}/v1/blog/posts`)
       .then(({ data }) => {
-        setDataBlog(data.data);
+        dispatch({ type: 'UPDATE_DATA_BLOG', payload: data.data });
+        // setDataBlog(data.data);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="home-page-wrapper">
@@ -29,12 +35,13 @@ const Home = () => {
           onClick={() => history.push('/create-blog')}
         />
       </div>
+      <p> {name}</p>
       <Gap height={20} />
       <div className="content-wrapper">
-        {dataBlog.map(blog => (
+        {dataBlogs.map(blog => (
           <BlogItem
             key={blog._id}
-            image={`http://localhost:4000/${blog.image}`}
+            image={`${process.env.REACT_APP_URL}/${blog.image}`}
             title={blog.title}
             body={blog.body}
             name={blog.author.name}
@@ -42,6 +49,7 @@ const Home = () => {
           />
         ))}
       </div>
+
       <Gap height={20} />
       <div className="pagination">
         <Button title="Previous" />
@@ -54,3 +62,12 @@ const Home = () => {
 };
 
 export default Home;
+
+// DOCS
+
+// * kita destcuring state global dari store nya
+// const stateGlobal = useSelector(state => state);
+// const { dataBlogs, name } = useSelector(state => state);
+
+// jadi kita gunakan state global
+// dataBlog dan setDataBlog kita hapus
