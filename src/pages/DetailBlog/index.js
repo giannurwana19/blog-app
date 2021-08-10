def7125bfc3bde3d25f9f1b595166555efb56635
giannurwana19 <giannurwana19@gmail.com>
2021-08-10 14:15:09 +0700

    styling detail blog page

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { RegisterBg } from '../../assets';
+import './detailBlog.scss';
 
 const DetailBlog = () => {
   return (
-    <div>
-      <p>Detail blog</p>
+    <div className="detail-blog-wrapper">
+      <img src={RegisterBg} className="img-cover" alt="thumb" />
+      <p className="blog-title">Title blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">
+        Content Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Distinctio dolor temporibus labore in rerum, quos harum totam officiis
+        ad, obcaecati a assumenda excepturi veritatis voluptatum itaque iure
+        praesentium doloribus laborum!
+      </p>
     </div>
   );
 };
