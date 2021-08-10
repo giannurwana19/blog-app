beaaeaec9d75a26f9fbc4f9ea6a9235375b37c14
giannurwana19 <giannurwana19@gmail.com>
2021-08-10 14:00:30 +0700

    styling create blog post page

---
+++ src/components/atoms/TextArea/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './textarea.scss';
+
+const TextArea = () => {
+  return (
+    <div>
+      <textarea className="text-area"></textarea>
+    </div>
+  );
+};
+
+export default TextArea;
--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Button, Gap, Input, TextArea, Upload } from '../../components';
+import './createBlog.scss';
 
 const CreateBlog = () => {
   return (
-    <div>
-      <p>content create blog</p>
+    <div className="blog-post">
+      <p className="title">Create New Blog</p>
+      <Input label="Post Title" />
+      <Upload />
+      <TextArea />
+      <Gap height={20} />
+      <div className="button-action">
+        <Button title="Save" />
+      </div>
+      <Gap height={20} />
     </div>
   );
 };
